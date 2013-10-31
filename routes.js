0cb585f211649ab96783b9e81667ea4a4ba2bd88
kyle <244098979@qq.com>
2013-10-31 17:35:09 +0800

    add test

--- routes.js
+++ routes.js
@@ -14,4 +14,32 @@ module.exports = function(app) {
 		});
 	});
 
+	app.get('/test2', function(req, res) {
+		res.render('test2', {
+			title: "微糖"
+		});
+	});
+
+	app.get('/divider', function(req, res) {
+		res.render('divider', {
+			title: "Divider"
+		});
+	});
+
+	app.get('/header', function(req, res) {
+		res.render('header', {
+			title: "header"
+		});
+	});
+
+	app.get('/icon', function(req,res) {
+		res.render('icon', {
+			title: "icon"
+		});
+	});
+
+
+
+	
+
 }
